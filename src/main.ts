4bcb4e642ba1b965c14772cc8d2cfdaa9163f54f
sebastian <sebastian.iwanczyszyn@gmail.com>
2025-05-14 18:52:16 +0200

    setup default env for gpubsub

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+process.env.PUBSUB_PROJECT_ID='test-project'
+process.env.PUBSUB_EMULATOR_HOST='localhost:8085'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
