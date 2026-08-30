e05da72aeb225947ad1d6cbe7c208bab0c0edcd6
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-08-30 22:37:55 +0200

    Bump

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    root: './src',
+    root: './test',
     include: ['**/*.spec.ts'],
     environment: 'node',
   },
